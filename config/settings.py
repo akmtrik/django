fb3a40ce8314f1fe5a83d09390a982a0ff90ecf4
akmtrik <s171001@kashiwanoha.ed.jp>
2021-09-22 07:10:18 +0000

    postgre

--- config/settings.py
+++ config/settings.py
@@ -66,8 +66,12 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # データベース設定
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR,'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'private_diary',
+        'USER': '',
+        'PASS': 'root',
+        'HOST':'',
+        'PORT':'',
     }
 }
 
